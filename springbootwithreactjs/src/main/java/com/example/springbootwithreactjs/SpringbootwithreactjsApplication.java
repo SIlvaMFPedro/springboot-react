6bf37d502e18deb17b5832f746a5bd27dd2d500c
silvamfpedro <silva.mfpedro@gmail.com>
2021-11-10 18:47:20 +0000

    Added first source files for Springboot with React JavaScript Application

---
+++ springbootwithreactjs/src/main/java/com/example/springbootwithreactjs/SpringbootwithreactjsApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootwithreactjs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootwithreactjsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootwithreactjsApplication.class, args);
+    }
+
+}
---
+++ springbootwithreactjs/src/test/java/com/example/springbootwithreactjs/SpringbootwithreactjsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootwithreactjs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootwithreactjsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
