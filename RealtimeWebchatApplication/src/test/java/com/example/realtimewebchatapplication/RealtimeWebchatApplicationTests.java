ac229037adbc783876a799e853ea9b692cfe03d5
silvamfpedro <silva.mfpedro@gmail.com>
2021-11-15 17:31:23 +0000

    Added first source files for Realtime Web Chat Java Application

---
+++ RealtimeWebchatApplication/src/test/java/com/example/realtimewebchatapplication/RealtimeWebchatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.realtimewebchatapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealtimeWebchatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
