1af97e365b5a6a87e568b04e2b4b56b5d408cb73
silvamfpedro <silva.mfpedro@gmail.com>
2022-02-08 15:28:25 +0000

    Added changes in the frontend module

--- springboot-crud-reactjs/react-frontend/src/App.js
+++ springboot-crud-reactjs/react-frontend/src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
+import React from "react";
+import logo from './assets/logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import ListEmployeeComponent from "./components/ListEmployeeComponent";
+import HeaderComponent from "./components/HeaderComponent";
+import FooterComponent from "./components/FooterComponent";
+import CreateEmployeeComponent from "./components/CreateEmployeeComponent";
+import UpdateEmployeeComponent from "./components/UpdateEmployeeComponent";
+import ViewEmployeeComponent from "./components/ViewEmployeeComponent";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <Router>
+          <HeaderComponent/>
+          <div className="container">
+            <Routes>
+              <Route path="/" exact component={ListEmployeeComponent}/>
+              <Route path="/employees" component={ListEmployeeComponent}/>
+              <Route path="/add-employee/:id" component={CreateEmployeeComponent}/>
+              <Route path="/view-employee/:id" component={ViewEmployeeComponent}/>
+              {/* <Route path = "/update-employee/:id" component = {UpdateEmployeeComponent}></Route> */}
+            </Routes>
+          </div>
+          <FooterComponent/>
+        </Router>
+      </div>
   );
 }
 
